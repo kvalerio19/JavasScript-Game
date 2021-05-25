56ba9557c20f8fd92a6387040ddddbb8b25f1fde
kvalerio19 <kvalerio19@hotmail.es>
2021-05-24 21:03:24 -0600

    Function Reset Game

--- script.js
+++ script.js
@@ -52,5 +52,23 @@ function setGameOver() {
 }
 
 function resetGame() {
+    guessCount = 1;
+
+    const resetParas = document.querySelectorAll();
+    for(let i = 0; i < resetParas.length; i ++){
+        resetParas[i].textContent = '';
+
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+    
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'white';
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
     
 }
